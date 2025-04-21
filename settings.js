932d285cc2c90a2870b65b1576c2ac434d930a80
DzinDev01 <dzingithub01@gmail.com>
2025-04-22 06:22:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const chalk = require('chalk') 
 
-global.owner = '6283139033735'
-global.owners = ['6283139033735']
+global.owner = '6283854545783'
+global.owners = ['6283854545783','6283142823119']
 global.namaOwner = "Dzin"
 global.packname = '© 2025' 
 global.author = 'ExlagMD'
